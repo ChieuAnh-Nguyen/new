4ba7dddc19b4ca01b211e1cdff6fdd4d0c9d5d7a
Chieu Anh Nguyen <chieuanh.nguyen@sjsu.edu>
2020-03-06 23:40:35 -0800

    Lab 2

---
+++ Flashcards/CreationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CreationViewController.swift
+//  Flashcards
+//
+//  Created by Chewy nguyen on 3/6/20.
+//  Copyright © 2020 Chieu Anh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var answerTextField: UITextField!
+    var flashcardsController: ViewController!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+    }
+    
+    @IBAction func didTapOnCancel(_ sender: Any) {
+    
+        
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        let questionText = questionTextField.text
+        let answerText = answerTextField.text
+    
+        flashcardsController.updateFlashcard(Question: questionText! , Answer: answerText!)
+        dismiss(animated: true)
+        
+    }
+    
+
+}
--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,18 +12,39 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
-    
+    @IBOutlet weak var card: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        card.layer.cornerRadius = 20
+        card.layer.shadowRadius = 15
+        card.layer.shadowOpacity = 5
+        
+        frontLabel.layer.cornerRadius = 20
+        frontLabel.clipsToBounds = true
+        backLabel.layer.cornerRadius = 20
+        backLabel.clipsToBounds = true
+//
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         print("Hello")
         frontLabel.isHidden = true
-        backLabel.isHidden = false
         backLabel.backgroundColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
+        
     }
+    func updateFlashcard(Question: String, Answer: String) {
+        backLabel.text = Answer
+        frontLabel.text = Question
     
-}
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        creationController.flashcardsController = self
+
+
+}
+}
